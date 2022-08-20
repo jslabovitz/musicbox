8b14310afd61ac02e31ff1c57573083bbe091512
John Labovitz <johnl@johnlabovitz.com>
2022-08-20 18:23:30 +0200

    Rebuild player.

--- lib/musicbox/commands/play.rb
+++ lib/musicbox/commands/play.rb
@@ -4,6 +4,24 @@ class MusicBox
 
     class Play < SimpleCommand::Command
 
+      Keymap = {
+        'a' => :play_random_album,
+        't' => :play_random_tracks,
+        'r' => :play_album_for_current_track,
+        'n' => :skip_to_next_track,
+        'p' => :skip_to_previous_track,
+        ' ' => :toggle_pause,
+        '<' => :skip_backward,
+        '>' => :skip_forward,
+        '^' => :skip_to_beginning,
+        's' => :stop,
+        'e' => :toggle_equalizer,
+        'E' => :next_equalizer,
+        'q' => :quit,
+        '.' => :show_playlist,
+        '?' => :show_keymap,
+      }
+
       attr_accessor :audio_device
       attr_accessor :audio_exclusive
       attr_accessor :mpv_log_level
@@ -11,21 +29,233 @@ class MusicBox
       attr_accessor :ignore_state
 
       def run(args)
+        @playlists = Playlists.new(root: $musicbox.playlists_dir)
+
         if @eq
-          equalizers = Equalizer.load_equalizers(
-            dir: $musicbox.equalizers_dir,
-            name: @eq)
+          @equalizers = Equalizer.load_equalizers(dir: $musicbox.equalizers_dir, name: @eq)
+          @equalizer_enabled = true
         else
-          equalizers = nil
+          @equalizers = []
         end
-        player = Player.new(
-          albums: $musicbox.find_albums(args),
-          equalizers: equalizers,
+
+        # if @ignore_state
+        #   @playlist.reset
+        # else
+        #   begin
+        #     @playlist = Playlist.restore(NAME)
+        #   rescue Error => _
+        #     reset_state
+        #   end
+        # end
+
+        @dispatcher = IO::Dispatcher.new
+        setup_interface
+        @player = Player.new(
           audio_device: @audio_device,
           audio_exclusive: @audio_exclusive,
           mpv_log_level: @mpv_log_level,
-          ignore_state: @ignore_state)
-        player.play
+          ignore_state: @ignore_state,
+          dispatcher: @dispatcher,
+          delegate: self)
+        @dispatcher.set_timeout_handler(10) do
+          # update_playlist
+          # @delegate&.checkpoint
+        end
+        next_equalizer
+        @dispatcher.run
+      end
+
+      def setup_interface
+        @stty_old_params = `stty -g`.chomp
+        at_exit {
+          system('stty', @stty_old_params) if @stty_old_params
+          @player.shutdown
+        }
+        system('stty', 'cbreak', '-echo')
+        @dispatcher.add_io_handler(input: STDIN) { |io| handle_key(io.sysread(1)) }
+      end
+
+      def handle_key(key)
+        case @key_state
+        when nil
+          if key == "\e"
+            @key_state = :esc
+          elsif (command = Keymap[key])
+            puts command_description(command)
+            if @player.respond_to?(command)
+              begin
+                @player.send(command)
+              rescue Player::RangeError => e
+                puts "error: #{e}"
+              end
+            elsif respond_to?(command)
+              begin
+                send(command)
+              rescue Error => e
+                puts "error: #{e}"
+              end
+            else
+              puts "unknown command: %p" % command
+            end
+          else
+            puts "unknown key: %p" % key
+          end
+        when :esc
+          @key_state = :left_bracket
+        when :left_bracket
+          case key
+          when 'A'  # up
+            @player.increase_volume
+          when 'B'  # down
+            @player.decrease_volume
+          else
+            puts "unknown escape key: %p" % key
+          end
+          @key_state = nil
+        else
+          raise Error, "Unknown state: #{@key_state.inspect}"
+        end
+      end
+
+      def key_description(key)
+        case key
+        when ' '
+          'space'
+        else
+          key
+        end
+      end
+
+      def command_description(command)
+        command.to_s.gsub('_', ' ')
+      end
+
+      def play(playlist)
+        @playlist = playlist
+        @playlists << @playlist
+        @playlist.save
+        @player.start(@playlist.paths)
+      end
+
+      #
+      # command callbacks
+      #
+
+      def play_random_album
+        play(Playlist.playlist_for_random_album(id: 'album'))
+      end
+
+      def play_random_tracks
+        play(Playlist.playlist_for_random_tracks(id: 'random-tracks', time: 60))
+      end
+
+      def play_album_for_current_track
+        raise Error, "No current playlist" unless @playlist
+        play(@playlist.playlist_for_album_of_current_track(id: 'album'))
+      end
+
+      def show_keymap
+        Keymap.each do |key, command|
+          puts "%-8s %s" % [key_description(key), command_description(command)]
+        end
+      end
+
+      def show_playlist
+        if @playlist
+          @playlist.tracks.each_with_index do |track, i|
+            puts '%1s %-40.40s | %-40.40s | %-40.40s' % [
+              (track == @playlist.current_track) ? '>' : ' ',
+              track.title,
+              track.album,
+              track.artist,
+            ]
+          end
+        end
+      end
+
+      def toggle_equalizer
+        @equalizer_enabled = !@equalizer_enabled
+        set_equalizer
+      end
+
+      def next_equalizer
+        if @equalizers
+          @equalizer &&= @equalizers[@equalizers.index(@equalizer) + 1]
+          @equalizer ||= @equalizers.first
+          if @equalizer
+            @player.set_audio_filter(@player.set_equalizer(@equalizer))
+            puts "equalizer: %s <%s>" % [
+              @equalizer&.name || 'none',
+              @equalizer_enabled ? 'enabled' : 'disabled',
+            ]
+          end
+        end
+      end
+
+      def quit
+        Kernel.exit(0)
+      end
+
+      #
+      # delegate methods
+      #
+
+      def state_did_change(state)
+        puts "STATE: #{state}"
+      end
+
+      def playlist_pos_did_change(value)
+        if @playlist
+          @playlist.pos = value
+          @playlist.save
+          track_did_change
+        end
+      end
+
+      def time_pos_did_change(value)
+        if @playlist
+          @playlist.time_pos = value
+          # @playlist.save if @playlist.age > 10
+        end
+      end
+
+      def volume_did_change(value)
+        puts "VOLUME: #{value}"
+      end
+
+      def track_did_change
+        track = @playlist.current_track
+        unless track
+          puts 'no current track playing'
+          return
+        end
+        message = track.title
+        subtitle = [track.artist, track.title].join(': ')
+        script = <<~END
+          display notification "#{message}" with title "MusicBox" subtitle "#{subtitle}"
+        END
+        run_command('osascript', input: script)
+        system('clear')
+        if track.cover
+          MusicBox.show_image(
+            file: track.cover,
+            width: 'auto',
+            height: 20,
+            preserve_aspect_ratio: false)
+          puts
+        end
+        Simple::Printer.print(
+          ['Track', [track.track_num, track.track_count].join('/')],
+          ['Title', track.title],
+          ['Album', track.album],
+          ['Artist', track.artist],
+        )
+        puts
+        show_playlist
+      end
+
+      def checkpoint
+        @playlist&.save
       end
 
     end
--- lib/musicbox/player.rb
+++ lib/musicbox/player.rb
@@ -2,467 +2,192 @@ class MusicBox
 
   class Player
 
-    Keymap = {
-      'a' => :play_random_album,
-      't' => :play_random_tracks,
-      'r' => :play_album_for_current_track,
-      'n' => :play_next_track,
-      'p' => :play_previous_track,
-      ' ' => :toggle_pause,
-      '<' => :skip_backward,
-      '>' => :skip_forward,
-      '^' => :skip_to_beginning,
-      's' => :stop,
-      'e' => :toggle_equalizer,
-      'E' => :next_equalizer,
-      'q' => :quit,
-      '.' => :show_playlist,
-      '?' => :show_keymap,
-    }
+    class RangeError < Error; end
 
-    attr_accessor :albums
     attr_accessor :audio_device
     attr_accessor :audio_exclusive
     attr_accessor :mpv_log_level
-    attr_accessor :equalizers
-    attr_accessor :ignore_state
-    attr_accessor :checkpoint_timeout
-    attr_accessor :state_file
-    attr_accessor :playlist_file
     attr_accessor :seek_seconds
+    attr_accessor :volume
+    attr_accessor :replay_gain
+    attr_accessor :dispatcher
+    attr_accessor :delegate
 
     include SetParams
 
     def initialize(**params)
-      @play_state = :stopped
-      @playlist = nil
       set(
         {
           mpv_log_level: 'error',
-          checkpoint_timeout: 10,
-          state_file: '/tmp/musicbox.state.json',
-          playlist_file: '/tmp/musicbox.playlist.m3u8',
           seek_seconds: 30,
-          ignore_state: false,
+          volume: 100,
+          replay_gain: 'album',
         }.merge(params.compact)
       )
-      @equalizer_enabled = @equalizers && !@equalizers.empty?
-      build_track_paths
-    end
-
-    def state_file=(file)
-      @state_file = Path.new(file)
-    end
-
-    def playlist_file=(file)
-      @playlist_file = Path.new(file)
-    end
-
-    def play
-      @dispatcher = IO::Dispatcher.new
-      setup_interface
-      setup_mpv
-      next_equalizer
-      @dispatcher.set_timeout_handler(@checkpoint_timeout) { save_state }
-      unless @ignore_state
-        begin
-          restore_state
-        rescue Error => e
-          show_error('Invalid state')
-          reset_state
-        end
-      end
-      show_status('ready')
-      @dispatcher.run
-    end
-
-    def setup_mpv
       @mpv = MPVClient.new(
         'msg-level' => "all=#{@mpv_log_level}",
         'audio-device' => @audio_device,
         'audio-exclusive' => @audio_exclusive ? 'yes' : 'no',
+        'replaygain' => @replay_gain,
         'audio-display' => 'no',
         'vo' => 'null',
-        'volume' => 100)
-      at_exit { shutdown_mpv }
+        'volume' => @volume)
       @dispatcher.add_io_handler(input: @mpv.socket) { |io| @mpv.process_response }
-      @dispatcher.add_io_handler(exception: @mpv.socket) { |io| shutdown_mpv }
-      @mpv.register_event('log-message') { |e| show_log(e) }
+      @dispatcher.add_io_handler(exception: @mpv.socket) { |io| raise Error, "Exception on #{io}" }
+      @mpv.register_event('log-message') { |e| handle_log_message(e) }
       @mpv.command('request_log_messages', @mpv_log_level) if @mpv_log_level
-      @mpv.register_event('start-file') { |e| start_file(e) }
-      @mpv.register_event('playback-restart') { |e| playback_restart(e) }
-      @mpv.observe_property('playlist') { |n, v| playlist_changed(v) }
-      @mpv.observe_property('playlist-pos') { |n, v| playlist_pos_changed(v) }
-      @mpv.observe_property('pause') { |n, v| pause_changed(v) }
-      @mpv.observe_property('volume') { |n, v| volume_changed(v) }
+      @mpv.register_event('start-file') { |e| handle_start_file(e) }
+      @mpv.register_event('playback-restart') { |e| handle_playback_restart(e) }
+      @mpv.observe_property('playlist-pos') { |n, v| playlist_pos_did_change(v) }
+      @mpv.observe_property('time-pos') { |n, v| time_pos_did_change(v) }
+      @mpv.observe_property('pause') { |n, v| pause_did_change(v) }
+      @mpv.observe_property('volume') { |n, v| volume_did_change(v) }
+      set_state(:ready)
     end
 
-    def shutdown_mpv
+    def shutdown
       if @mpv
-        save_state
         @mpv.command('quit')
         @dispatcher.remove_io_handler(input: @mpv.socket, exception: @mpv.socket)
         @mpv.stop
         @mpv = nil
       end
+      @dispatcher = nil
     end
 
-    def setup_interface
-      @stty_old_params = `stty -g`.chomp
-      at_exit { system('stty', @stty_old_params) }
-      system('stty', 'cbreak', '-echo')
-      @dispatcher.add_io_handler(input: STDIN) { |io| handle_key(io.sysread(1)) }
+    def start(playlist, playlist_pos: nil, time_pos: nil)
+      @playlist = playlist
+      @playlist_pos = @time_pos = nil
+      playlist_path = Path.new('/tmp/mpv.playlist.m3u8')
+      playlist_path.write(@playlist.join("\n"))
+      @future_playlist_pos = (playlist_pos && playlist_pos >= 0) ? playlist_pos : nil
+      @future_time_pos = (time_pos && time_pos > 0) ? time_pos : nil
+      @mpv.command('loadlist', playlist_path.to_s)
     end
 
-    def save_state
-      state = {}
-      if (playlist = @mpv.get_property('playlist'))
-        state['playlist'] = playlist.map { |e| e['filename'] }
-        if (pos = @mpv.get_property('playlist-pos')) && pos >= 0
-          state['playlist-pos'] = pos
-          state['time-pos'] = @mpv.get_property('time-pos') rescue nil
-        end
-      end
-      @state_file.dirname.mkpath unless @state_file.dirname.exist?
-      @state_file.write(JSON.dump(state))
+    def stop
+      @mpv.command('stop')
+      set_state(:stopped)
     end
 
-    def restore_state
-      if state_file.exist?
-        state = JSON.load(state_file.read)
-        if state['playlist']
-          tracks = state['playlist'].map { |p| @track_paths[p] }.compact
-          unless tracks.empty?
-            play_tracks(tracks,
-              pos: state['playlist-pos'],
-              time: state['time-pos'])
-          end
-        end
-      end
+    def skip_to_next_track
+      raise RangeError, "No next track" unless next_track
+      @mpv.command('playlist-next')
     end
 
-    def reset_state
-      @state_file.unlink
+    def skip_to_previous_track
+      raise RangeError, "No previous track" unless previous_track
+      @mpv.command('playlist-prev')
     end
 
-    def play_tracks(tracks, pos: nil, time: nil)
-      @playlist_file.dirname.mkpath
-      @playlist_file.write(tracks.map(&:path).join("\n"))
-      @mpv.command('loadlist', @playlist_file.to_s)
-      @future_playlist_pos = pos if pos && pos >= 0
-      @future_time_pos = time if time && time > 0
+    def toggle_pause
+      raise RangeError, "No current track" unless current_track
+      @mpv.set_property('pause', !@mpv.get_property('pause'))
     end
 
-    def set_equalizer
-      show_equalizer
-      @mpv.command('af', 'set', @current_equalizer.to_af(@equalizer_enabled)) if @current_equalizer
+    def skip_backward
+      raise RangeError, "No current track" unless current_track
+      @mpv.command('seek', -@seek_seconds)
     end
 
-    def build_track_paths
-      @track_paths = {}
-      @albums.each do |album|
-        album.tracks.each do |track|
-          @track_paths[track.path.to_s] = track
-        end
-      end
+    def skip_forward
+      raise RangeError, "No current track" unless current_track
+      @mpv.command('seek', @seek_seconds)
+    end
+
+    def skip_to_beginning
+      raise RangeError, "No current track" unless current_track
+      @mpv.command('seek', 0, 'absolute-percent')
+    end
+
+    def increase_volume
+      @mpv.command('add', 'volume', 5)
+    end
+
+    def decrease_volume
+      @mpv.command('add', 'volume', -5)
+    end
+
+    def set_audio_filter(filter)
+      @mpv.command('af', 'set', filter)
+    end
+
+    private
+
+    def current_track
+      @playlist_pos && @playlist[@playlist_pos]
+    end
+
+    def next_track
+      @playlist_pos && @playlist[@playlist_pos + 1]
+    end
+
+    def previous_track
+      @playlist_pos && @playlist_pos > 0 && @playlist[@playlist_pos - 1]
+    end
+
+    def set_state(state)
+      @delegate&.state_did_change(state)
     end
 
     #
     # callbacks for events
     #
 
-    def start_file(event)
-# ;;puts "EVENT: #{event.inspect}"
-      if @future_playlist_pos
-        @mpv.set_property('playlist-pos', @future_playlist_pos)
-        @future_playlist_pos = nil
-      end
-    end
-
-    def playback_restart(event)
-# ;;puts "EVENT: #{event.inspect}"
-      if @future_time_pos
-        @mpv.set_property('time-pos', @future_time_pos)
-        @future_time_pos = nil
-      end
-      display_notification
-    end
-
-    #
-    # callbacks for properties
-    #
-
-    def playlist_changed(value)
-# ;;puts "PROPERTY: #{__method__} => #{value.inspect}"
-      @playlist = value
-      @current_track = @current_pos = nil
-      @playlist.each_with_index do |entry, i|
-        if entry['current']
-          @current_track = @track_paths[entry['filename']]
-          @current_pos = i
-          break
-        end
-      end
-      save_state
-      show_playlist
-    end
-
-    def playlist_pos_changed(value)
-# ;;puts "PROPERTY: #{__method__} => #{value.inspect}"
-      if value >= 0
-        entry = @mpv.get_property('playlist')[value] or raise
-        @current_track = @track_paths[entry['filename']]
-        @current_pos = value
-      else
-        @current_track = @current_pos = nil
-      end
-      save_state
-      show_current_track
-    end
-
-    def pause_changed(value)
-# ;;puts "PROPERTY: #{__method__} => #{value.inspect}"
-      @play_state = value ? :paused : :playing
-      show_play_state
-    end
-
-    def volume_changed(value)
-# ;;puts "PROPERTY: #{__method__} => #{value.inspect}"
-      puts "VOLUME: #{value}"
-    end
-
-    #
-    # commands called by interface
-    #
-
-    def quit
-      Kernel.exit(0)
-    end
-
-    def play_next_track
-      if @current_pos && @current_pos < @playlist.length - 1
-        @mpv.command('playlist-next')
-      else
-        show_status('no next track')
-      end
-    end
-
-    def play_previous_track
-      if @current_pos && @current_pos > 0
-        @mpv.command('playlist-prev')
-      else
-        show_status('no previous track')
-      end
-    end
-
-    def play_random_album
-      if @albums.empty?
-        show_status('no albums to play')
-      else
-        play_tracks(@albums.sample.tracks)
-      end
-    end
-
-    def play_random_tracks
-      if @albums.empty?
-        show_status('no albums to play')
-      else
-        tracks = Set.new
-        while tracks.length < 10
-          tracks << @albums.sample.tracks.sample
-        end
-        play_tracks(tracks.to_a)
-      end
-    end
-
-    def play_album_for_current_track
-      if @current_track
-        play_tracks(@current_track.album.tracks)
-      else
-        show_status('no current track')
-      end
-    end
-
-    def toggle_pause
-      if @current_track
-        @mpv.set_property('pause', !@mpv.get_property('pause'))
-      else
-        show_status('no current track')
-      end
-    end
-
-    def skip_backward
-      if @current_track
-        @mpv.command('seek', -@seek_seconds)
-      else
-        show_status('no current track')
-      end
-    end
-
-    def skip_forward
-      if @current_track
-        @mpv.command('seek', @seek_seconds)
-      else
-        show_status('no current track')
-      end
-    end
-
-    def skip_to_beginning
-      if @current_track
-        @mpv.command('seek', 0, 'absolute-percent')
-      else
-        show_status('no current track')
-      end
-    end
-
-    def stop
-      @mpv.command('stop')
-      reset_state
-      @play_state = :stopped
-    end
-
-    def display_notification
-      if (track = @current_track) && (album = track.album)
-        title = 'MusicBox'
-        subtitle = [track.artist_name || album.artist_name, album.title].join(': ')
-        message = track.title
-        run_command('osascript',
-          input: <<~END
-            display notification "#{message}" with title "#{title}" subtitle "#{subtitle}"
-          END
-        )
-      end
-    end
-
-    def show_current_track
-      if (track = @current_track)
-        album = track.album
-        if album.has_cover?
-          MusicBox.show_image(
-            file: album.cover_file,
-            width: 'auto',
-            height: 20,
-            preserve_aspect_ratio: false)
-          puts
-        end
-        Simple::Printer.print(
-          ['Disc', track.disc_num || '-'],
-          ['Track', [track.track_num, album.tracks.count].join('/')],
-          ['Title', track.title],
-          ['Album', album.title],
-          ['Artist', [album.artist_name, track.artist_name].compact.uniq.join(' / ')],
-        )
-        puts
-      end
-    end
-
-    def show_playlist
-      if @playlist
-        @playlist.each_with_index do |entry, i|
-          track = @track_paths[entry['filename']]
-          album = track.album
-          puts '%1s%1s %2d. %-40.40s | %-40.40s | %-40.40s' % [
-            entry['current'] ? '=' : '',
-            entry['playing'] ? '>' : '',
-            i + 1,
-            track.title,
-            album.title,
-            album.artist_name,
-          ]
-        end
-      else
-        show_status('no current playlist')
-      end
-      puts
-    end
-
-    def show_equalizer
-      puts "EQUALIZER: %s <%s>" % [
-        @current_equalizer&.name || 'none',
-        @equalizer_enabled ? 'enabled' : 'disabled',
-      ]
-    end
-
-    def show_play_state
-      puts "PLAY STATE: #{@play_state}"
-    end
-
-    def show_error(msg)
-      puts "ERROR: #{msg}"
-    end
-
-    def show_status(status)
-      puts "STATUS: #{status}"
-    end
-
-    def show_log(event)
-      puts "LOG: %-6s | %-20s | %s" % [
+    def handle_log_message(event)
+      warn "LOG: %-6s | %-20s | %s" % [
         event['level'],
         event['prefix'],
         event['text'].strip,
       ]
     end
 
-    def show_keymap
-      Keymap.each do |key, command|
-        puts "%-8s %s" % [key_description(key), command_description(command)]
-      end
-    end
-
-    def toggle_equalizer
-      @equalizer_enabled = !@equalizer_enabled
-      set_equalizer
-    end
-
-    def next_equalizer
-      if @equalizers
-        @current_equalizer &&= @equalizers[@equalizers.index(@current_equalizer) + 1]
-        @current_equalizer ||= @equalizers.first
-        set_equalizer
-      end
-    end
-
-    private
-
-    def handle_key(key)
-      case @key_state
-      when nil
-        if key == "\e"
-          @key_state = :esc
-        elsif (command = Keymap[key])
-          show_status(command_description(command))
-          send(command)
-        else
-          show_error("unknown key: %p" % key)
-        end
-      when :esc
-        @key_state = :left_bracket
-      when :left_bracket
-        case key
-        when 'A'  # up
-          @mpv.command('add', 'volume', 5)
-        when 'B'  # down
-          @mpv.command('add', 'volume', -5)
-        else
-          show_error("unknown escape key: %p" % key)
-        end
-        @key_state = nil
+    def handle_start_file(event)
+# ;;warn "EVENT: #{event.inspect}"
+      if @future_playlist_pos
+        @mpv.set_property('playlist-pos', @future_playlist_pos)
+        @future_playlist_pos = nil
       else
-        raise Error, "Unknown state: #{@key_state.inspect}"
+        @playlist_pos = @mpv.get_property('playlist-pos')
+        @delegate&.playlist_pos_did_change(@playlist_pos)
       end
     end
 
-    def key_description(key)
-      case key
-      when ' '
-        'space'
+    def handle_playback_restart(event)
+# ;;warn "EVENT: #{event.inspect}"
+      if @future_time_pos
+        @mpv.set_property('time-pos', @future_time_pos)
+        @future_time_pos = nil
       else
-        key
+        @time_pos = @mpv.get_property('time-pos')
+        @delegate&.time_pos_did_change(@time_pos)
       end
     end
 
-    def command_description(command)
-      command.to_s.gsub('_', ' ')
+    #
+    # callbacks for properties
+    #
+
+    def playlist_pos_did_change(value)
+# ;;warn "PROPERTY: #{__method__} => #{value.inspect}"
+      @playlist_pos = (value >= 0) ? value : nil
+      @delegate&.playlist_pos_did_change(@playlist_pos)
+    end
+
+    def time_pos_did_change(value)
+# ;;warn "PROPERTY: #{__method__} => #{value.inspect}"
+      @time_pos = value
+      @delegate&.time_pos_did_change(@time_pos)
+    end
+
+    def pause_did_change(value)
+# ;;warn "PROPERTY: #{__method__} => #{value.inspect}"
+      set_state(value ? :paused : :playing)
+    end
+
+    def volume_did_change(value)
+# ;;warn "PROPERTY: #{__method__} => #{value.inspect}"
+      @delegate&.volume_did_change(value)
     end
 
   end
